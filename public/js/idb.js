63d8366a5cf9964270404f912e783f93878876d6
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-28 14:35:49 -0800

    uploadBudget function

--- public/js/idb.js
+++ public/js/idb.js
@@ -39,4 +39,47 @@ request.onsuccess = function(event) {
     pizzaObjectStore.add(record);
   }
 
+  function uploadBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_budget'], 'readwrite');
   
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/budget', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          // access the new_budget object store
+          const budgetObjectStore = transaction.objectStore('new_budget');
+      
+          budgetObjectStore.clear();
+
+          alert('All saved budget values and transactions have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+  }
+
