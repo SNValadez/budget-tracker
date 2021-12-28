e022d4b26e174f48c5eaebeb30dd01b7cf545a96
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-28 15:46:45 -0800

    debugging MONGO

--- server.js
+++ server.js
@@ -16,11 +16,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+  // mongoose.connect(MONGODB_URI, {
+  //   useNewUrlParser: true,
+  //   useFindAndModify: false
+  // });
 
+mongoose.connect(MONGODB_URI, 
+  {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 // routes
 app.use(require("./routes/api.js"));
 
