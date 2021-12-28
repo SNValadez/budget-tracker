7e727bc822bc4e301fae254de5f1529917c7b023
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-28 12:36:12 -0800

    service installation

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,6 @@
 console.log("Hi from your service-worker.js file!!");
+
+
 const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
@@ -31,11 +33,20 @@ self.addEventListener('fetch', function (e) {
           console.log('responding with cache : ' + e.request.url)
           return request
 
-          
+
         } else {       // if there are no cache, try fetching request
           console.log('file is not cached, fetching : ' + e.request.url)
           return fetch(e.request)
         }
+      })
+    )
+  })
+
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
       })
     )
   })
