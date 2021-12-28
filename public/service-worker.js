e4d173aee2b023c300e44c5ddbd9f2d1265f157a
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2021-12-28 12:32:43 -0800

    const files and prefixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
 console.log("Hi from your service-worker.js file!!");
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./events.html",
+  "./tickets.html",
+  "./schedule.html",
+  "./assets/css/style.css",
+  "./css/styles.css",
+  "./icons/icon-96x96.png",
+  "./icons/icon-72x72.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./manifest.json",
+  "./js/idb.js",
+  "./js/index.js"
+];
+
